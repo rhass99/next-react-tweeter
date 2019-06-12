ec790e3a7da3e8c813aed6bd9a667161c34bf86d
rhass99 <rhass99@gmail.com>
2019-06-12 21:58:23 +0000

    git case sensetive

---
+++ components/styledComponents/components.js
@@ -0,0 +1,19 @@
+import { Button, Segment } from "semantic-ui-react";
+import styled from "styled-components";
+
+export const HeaderButton = styled(Button)`
+&&& {
+  background-color: #49C39E;
+}
+`;
+
+export const TweetButton = styled(Button)`
+&&& {
+  background-color: #49C39E;
+}
+`
+export const CreateTweetSegment = styled(Segment)`
+&&& {
+  background-color: #EBB424;
+}
+`
